b3d251ee5e437de6049a2d2b5f44825b510bfc45
kgweicat <37817830+kgweicat@users.noreply.github.com>
2020-05-22 07:56:51 -0500

    upload rest of files

---
+++ Magazine.java
@@ -0,0 +1,73 @@
+
+import linkedlist.IDedObject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Che-Wei Chou
+ */
+public class Magazine implements IDedObject {
+
+    private int magazineID;
+    private String magazineName;
+    private String publisherName;
+
+    /**
+     * Construct a magazine object with the given parameters
+     *
+     * @param id
+     * @param magazine
+     * @param publisher
+     */
+    public Magazine(int id, String magazine, String publisher) {
+        magazineID = id;
+        magazineName = magazine;
+        publisherName = publisher;
+    }
+
+    /**
+     * Print out the ID of the magazine.
+     */
+    @Override
+    public void printID() {
+        System.out.println(magazineID);
+    }
+
+    /**
+     * get the id.
+     *
+     * @return the id of the magazine.
+     */
+    @Override
+    public int getID() {
+        return magazineID;
+    }
+
+    public String magazineName() {
+        return magazineName;
+    }
+
+    public String publisherName() {
+        return publisherName;
+    }
+
+    /**
+     * toString
+     *
+     * @return magazine info formatted in
+     * magazineID\nmagazineName\npublishedName
+     */
+    @Override
+    public String toString() {
+        String result = "";
+        result += magazineID + "\n";
+        result += magazineName + "\n";
+        result += publisherName + "\n";
+        return result;
+    }
+
+}
