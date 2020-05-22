3db87ff68ed4475c169993677609a3c6de7053b1
kgweicat <37817830+kgweicat@users.noreply.github.com>
2020-05-22 07:46:05 -0500

    Organize comments

--- linkedlist/IDedObject.java
+++ linkedlist/IDedObject.java
@@ -1,16 +1,9 @@
 package linkedlist;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Che-Wei Chou
+ * @author kgweicat
  */
-//an interface
 public interface IDedObject {
     
     void printID();
